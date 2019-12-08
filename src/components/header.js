da6a4af84d6818a666404961eda3abd295558adb
ayan4m1 <andrew@bulletlogic.com>
2019-12-08 01:03:21 -0500

    remove superfluous header nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 const Header = ({ siteTitle }) => (
   <Navbar>
     <Navbar.Brand>{siteTitle}</Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Nav>
-        <Nav.Link as={Link} to="/" activeLinkKey="/">
-          View Stash
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
   </Navbar>
 );
 
